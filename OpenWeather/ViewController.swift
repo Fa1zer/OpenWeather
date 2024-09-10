a15e62f2ff46e863998c86e6c34972e2cc49f27e
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2024-09-10 22:54:49 +0300

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Artemiy Zuzin on 10.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
